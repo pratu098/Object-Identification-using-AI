d3c3120db078255b3de621be979bf9ebee9d1d8b
pratu098 <pratiksha6294@gmail.com>
2023-03-23 17:41:01 +0530

    filesgit rm -r --cached myFolder

--- imgUpload/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,re_path
-from . import views
-
-urlpatterns = [
-    re_path(r'^$', views.imageUpload, name='imageUpload'),
-    re_path(r'imageprocess', views.imageprocess, name= 'imageprocess')
-]
